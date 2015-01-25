3767bf7f92d8eb699ece0ad7f3793fe0cd638e83
Michael Walther <rundungsfehler@gmail.com>
2015-01-25 14:26:50 +0100

    First Try

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To avoid costly re-computation of large inverse matrices, it is possible to cache the inverse matrix. 
+## This is done by creating an object like environment (makeCacheMatrix) where the inverted matrix can be stored,
+## together with functions to access (read, write) the original matrix as well as the inverse matrix.
 
-## Write a short comment describing this function
+## -----------------------------------------------------------------------------------------------------------------
+## makeCacheMatrix implements the access functions for the matrices (mat,invertedMatrix) as well as the environment
+## to store both matrices. 
+## makeCacheMatrix returns a list of access functions.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## setMatrix: stores the matrix and initializes invertedMatrix.
+## setInvertedMatrix:  stores invertedMatrix.
+## getMatrix: retrieves the matrix.
+## getInvertedMatrix: retrieves the invertedMatrix.
+## -----------------------------------------------------------------------------------------------------------------
+makeCacheMatrix <- function(mat = numeric()) {
+    invertedMatrix <- NULL
+    ## set and get matrix mat      
+    setMatrix <- function(m) {
+        mat <<- m
+        invertedMatrix <<- NULL
+    }
+    
+    getMatrix <- function() mat
+    
+    ## set and get inverted Matrix
+    setInvertedMatrix <- function(inverted) invertedMatrix <<- inverted
+    
+    getInvertedMatrix <- function() invertedMatrix
+    
+    ## return set and get functions for matrix mat and invertedMatrix
+    list(setMatrix = setMatrix,
+         getMatrix = getMatrix,
+         setInvertedMatrix = setInvertedMatrix,
+         getInvertedMatrix = getInvertedMatrix)
 }
 
-
-## Write a short comment describing this function
-
+## -----------------------------------------------------------------------------------------------------------------
+## cacheSolve checks whether the inverted matrix is already calculated, if so, invertedMatrix is returned
+## if not, invertedMatrix is calculated, cached and returned
+## It is assumed that the original matrix can be inverted. 
+## -----------------------------------------------------------------------------------------------------------------
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    invertedMatrix <- x$getInvertedMatrix()
+    
+    if(!is.null(invertedMatrix)) {
+        message("getting cached data")
+        return(invertedMatrix)
+    }
+    data <- x$getMatrix()
+    invertedMatrix <- solve(data)
+    x$setInvertedMatrix(invertedMatrix)
+    invertedMatrix
 }
